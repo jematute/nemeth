b7fb3c85a72fed0d0417dd14f8aee42539f35a30
Juan Matute <Juan@Juans-MacBook-Pro.local>
2016-04-16 00:32:56 -0400

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
     // configuration =================
 
     mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
-
+	app.set('port', (process.env.PORT || 5000));
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -23,5 +23,6 @@
 	    });
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
+		app.listen(app.get('port'), function() {
+		  console.log('Node app is running on port', app.get('port'));
+		});
